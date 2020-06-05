a189dea16fa2950793d5c2557d04f6392d67a38e
Aquinas <aquinasMT1016@gmail.com>
2020-06-05 09:16:33 -0500

    user interface with working dashboard main panel

--- WordPredictor/ui.R
+++ WordPredictor/ui.R
@@ -7,32 +7,77 @@
 #    http://shiny.rstudio.com/
 #
 
-shinyUI(fluidPage(
-    titlePanel("Predicting the Next Word"),
-    
-    fluidRow(
-        column(12,
-               br(),
-               h4("To run the application, type a phrase in the box below."),
-               br(),
-               br()
-        )
-    ),
-    
-    fluidRow(
-        column(6,
-               textInput("input_str", 
-                         label = "Enter some text without punctuation:", 
-                         value = " "
-               )             
-        )    
-    ),
-    fluidRow(
-        column(12,
-               br(),
-               br(),
-               h4("Predicted next word:", style = "color:blue"), 
-               verbatimTextOutput("text")            
-        )
+library(shiny)
+library(shinyWidgets)
+library(shinycssloaders)
+library(shinydashboard)
+
+dashboardPage(
+    dashboardHeader(title = "Aquinas Text Prediction Project"),
+    dashboardSidebar(),
+    dashboardBody(
+        fluidRow(
+            column(12,
+                   br(),
+                   h4("Enter a short prediction phrase below, and the app will search the corpus for the best next word:"),
+            )
+        ),
+        
+        fluidRow(
+            column(6,
+                   textInput("input_str", 
+                             label = "(no punctuation please)", 
+                             value = " ")
+                   )             
+            ),
+
+            mainPanel(
+                h4("Predicted Word"),
+                
+                verbatimTextOutput("text") %>% withSpinner(color="#290902")            
+            )
+            
     )
-))
+)
+
+
+#shinyUI(fluidPage(
+#    setBackgroundColor(
+#        color = c("#F7FBFF", "#D2B271"),
+#        gradient = "radial",
+#        direction = c("top", "left")
+#    ),
+#    
+
+#    titlePanel(h1("Aquinas' Text Predictor")),
+#    tags$h1(tags$style(".titlePanel{ 
+#                         color: red;
+#                         font-size: 20px;
+#                         font-style: italic;
+#                         }")),
+    
+#    fluidRow(
+#        column(12,
+#               br(),
+#               h4("Enter a short prediction phrase below, and the app will search the corpus for the best next word:"),
+#                )
+#            ),
+#    
+#    fluidRow(
+#        column(6,
+#               textInput("input_str", 
+#                         label = "(no punctuation please)", 
+#                         value = " "
+#                           )             
+#            )    
+    
+#    ),
+    
+#    mainPanel(
+#       h4("Predicted Word"),
+        
+#         verbatimTextOutput("text") %>% withSpinner(color="#290902")            
+#         )
+    
+#    ))
+
